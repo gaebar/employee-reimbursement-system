4162b6f548c8034b195b35583ed87c62bfd8ea09
gaebar <gaetanobarreca@gmail.com>
2024-05-01 13:07:47 -0700

    Add ERSBackend project files

---
+++ ERSBackend/src/main/java/com/reimbursement/ERSBackend/ErsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reimbursement.ERSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsBackendApplication.class, args);
+	}
+
+}
