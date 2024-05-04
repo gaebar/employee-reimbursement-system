47b8ef120caeacb79cc75586a49ea6faceaefc3d
gaebar <gaetanobarreca@gmail.com>
2024-05-04 15:30:31 -0700

    Added new optional data to ReimbusementInterface

--- ers-frontend/src/interfaces/ReimbursementInterface.ts
+++ ers-frontend/src/interfaces/ReimbursementInterface.ts
@@ -3,6 +3,7 @@ export interface ReimbursementInterface {
     reimbursementId?: number;
     amount: number;
     description: string;
-    status: 'Pending' | 'Approved' | 'Denied';  // Using a union of string literals for status
-    userId: number;  // Assuming every reimbursement is linked to a user
+    status: 'Pending' | 'Approved' | 'Denied'; // Using a union of string literals for status
+    userId: number; // Assuming every reimbursement is linked to a user
+    dateSubmitted?: Date; // Optional date field added
 }
